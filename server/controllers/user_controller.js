044bae25ff2b0082f325009da7cab671d0582b27
Aidan Corcoran <acorcora@uoguelph.ca>
2023-12-22 19:22:43 -0500

    Following / unfollowing partially correct, will tune later

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -2,28 +2,77 @@ const User = require("../models/user_model");
 
 // Create a new user account
 const createUser = async (req, res) => {
-    const {Name, Email, Password} = req.body
+    const {Name, Email, Password} = req.body;
     try{
-        const isExistingUser = await User.findOne({Email: Email})
+        const isExistingUser = await User.findOne({Email: Email});
 
         if(isExistingUser) {
-            res.status(400).json("Error: User already exists")
+            res.status(400).json("Error: User already exists");
         }
         else {
-            const user = await User.create({Name, Email, Password})
-            res.status(200).json(user)
+            const user = await User.create({Name, Email, Password});
+            res.status(200).json(user);
         }
     } catch(error) {
-        res.status(400).json({error: error.message})
+        res.status(400).json({error: error.message});
     }
 } 
 
 // Follow another user
+const followUser = async (req, res) => {
+    const userToFollow = await User.findById(req.body.followId);
+    console.log(userToFollow);
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            req.body.followId,
+            {
+                $push: {Followers: userToFollow._id},
+            },
+            {
+                new: true,
+            }
+        );
+        console.log(updatedUser);
+        if(!updatedUser) {
+            res.status(400).json("User not found");
+        }
+
+        res.status(200).json(updatedUser);
+    } catch(error) {
+        console.error(error);
+        res.status(400).json({error: error.message});
+    }
+}
 
 // Unfollow another user
+const unfollowUser = async (req, res) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            req.body.followId,
+            {
+                $pull: {Followers: req.user._id},
+            },
+            {
+                new: true,
+            }
+        );
+
+        if(!updatedUser) {
+            res.status(400).json("User not found");
+        }
+
+        res.status(200).json(updatedUser);
+    } catch(error) {
+        console.error(error);
+        res.status(400).json({error: error.message});
+    }
+}
+
 
 // Search a user
 
 module.exports = {
-    createUser
-}
+    createUser,
+    followUser,
+    unfollowUser
+};
--- server/routes/post_req.js
+++ server/routes/post_req.js
@@ -1,17 +1,23 @@
 // Deal with post requests
 const userSchema = require("../models/user_model");
 const {
-    createUser
+    createUser,
+    followUser,
+    unfollowUser
 } = require("../controllers/user_controller");
 
 module.exports = (app) => {
     app.get("/home", (req, res) => {
-        res.json("Get users followers posts")
+        res.json("Get users followers posts");
     });
     
     app.get("/userposts", (req, res) => {
-        res.json("Get the user's posts and display in grid")
+        res.json("Get the user's posts and display in grid");
     });
 
-    app.post("/newuser", createUser)
+    app.post("/newuser", createUser);
+    
+    app.post("/follow", followUser);
+
+    app.post("/unfollow", unfollowUser);
 }
