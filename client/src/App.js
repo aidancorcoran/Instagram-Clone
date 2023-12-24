a492d5e2ebe9eeabd43b2cb600552bf4ab3103c0
Aidan Corcoran <acorcora@uoguelph.ca>
2023-12-24 17:16:34 -0500

    starting work on the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
 import React from "react";
 // We use Route in order to define the different routes of our application
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
  // We import all the components we need in our app
+import Home from "./pages/home"
 
 import Routing from "./routes/Routing";
 
  const App = () => {
  return (
    <div>
-     <Routes>
-        <Routing />
-     </Routes>
+     <BrowserRouter>
+      <div className="pages">
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+        </Routes>
+      </div>
+     </BrowserRouter>
    </div>
  );
 };
- export default App;
+
+export default App;
--- client/src/routes/Routing.js
+++ client/src/routes/Routing.js
@@ -1,15 +1,15 @@
-import { useContext, useEffect } from "react";
+// import { useContext, useEffect } from "react";
 
 
-const Routing = () => {
-    const {auth_state} = useContext(AuthContext);
+// const Routing = () => {
+//     const {auth_state} = useContext(AuthContext);
 
-    // Check if logged in
-    useEffect(() => {
-        auth_state.isAuthenticated ?
-    });
+//     // Check if logged in
+//     useEffect(() => {
+//         auth_state.isAuthenticated ?
+//     });
 
 
-}
+// }
 
-export default Routing;
+// export default Routing;
