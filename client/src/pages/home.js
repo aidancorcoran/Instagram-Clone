550d85195fbe588e73f0475ed05d561c0d1948b6
aidancorcoran <ajcorcoran2001@gmail.com>
2023-12-27 20:37:30 -0500

    frontend to backend communication

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,4 +1,22 @@
+import { useEffect, useState } from "react"
+
 const Home = () => {
+    const [posts, setPosts] = useState(null)
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const response = await fetch("http://localhost:5000/home")
+            const json = await response.json()
+
+            if(response.ok) {
+                setPosts(json)
+            }
+        }
+
+        fetchPosts()
+    }, [])
+
+
     return(
         <div className="home">
             <h2>Home</h2>
